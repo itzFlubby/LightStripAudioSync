1899f9f1d1a807b47b0c99353c9c0d0da097c0f4
Leonard <20861978+itzFlubby@users.noreply.github.com>
2026-01-28 23:59:36 +0100

    Adjusts parameters

--- AudioCapture.hpp
+++ AudioCapture.hpp
@@ -17,11 +17,11 @@ class AudioCapture {
         constexpr static unsigned AUTOSCALE_TIME_WINDOW_MS = 1000;
         constexpr static double AUTOSCALE_VALUE            = 0.95;
         constexpr static unsigned INPUT_BUFFER_SIZE        = 1024;
-        constexpr static double MAX_FREQUENCY              = 2000.;
-        constexpr static unsigned BINS_SIZE                = 20;
+        constexpr static double MAX_FREQUENCY              = 5000.;
+        constexpr static unsigned BINS_SIZE                = 30;
 
         constexpr static double ENVELOPE_FOLLOWER_ATTACK  = 0.99; // Perceived as delay when peak is rising (higher is faster)
-        constexpr static double ENVELOPE_FOLLOWER_RELEASE = 0.40; // Perceived as delay when peak is falling (higher is faster)
+        constexpr static double ENVELOPE_FOLLOWER_RELEASE = 0.30; // Perceived as delay when peak is falling (higher is faster)
 
         struct Bin {
             public:
